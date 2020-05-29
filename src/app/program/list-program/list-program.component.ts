ac0e82f97974c2cd98b03b91d52400d5f5695819
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-29 13:32:07 +0700

    add delete program

--- src/app/program/list-program/list-program.component.ts
+++ src/app/program/list-program/list-program.component.ts
@@ -3,6 +3,7 @@ import {Program} from "../../interface/program";
 import {ProgramService} from "../../service/program/program.service";
 
 declare var $: any;
+declare var Swal: any;
 
 @Component({
   selector: 'app-list-program',
@@ -11,12 +12,51 @@ declare var $: any;
 })
 export class ListProgramComponent implements OnInit {
   listProgram: Program[];
+  id: number;
 
   constructor(private programService: ProgramService) {
   }
 
   ngOnInit() {
     this.getAllProgram();
+  }
+
+  getProgramId(id: number) {
+    this.id = id;
+  }
+
+  deleteProgram() {
+    this.programService.deleteProgram(this.id).subscribe(() => {
+      this.programService.getAllProgram().subscribe(listProgram => {
+        this.listProgram = listProgram;
+      })
+      $(function () {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 3000
+        });
+        Toast.fire({
+          type: 'success',
+          title: 'Xóa thành công'
+        });
+      });
+    }, () => {
+      $(function () {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 3000
+        });
+
+        Toast.fire({
+          type: 'error',
+          title: 'Xóa thất bại'
+        });
+      });
+    })
   }
 
   getAllProgram() {
