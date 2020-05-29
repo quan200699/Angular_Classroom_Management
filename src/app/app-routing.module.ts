37f0d3333dc15c4ff2270222fe3c715c8531f0e3
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-29 14:08:59 +0700

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 
 
-const routes: Routes = [{
-  path: 'programs',
-  loadChildren: () => import('./program/program.module').then(module => module.ProgramModule)
-}];
+const routes: Routes = [
+  {
+    path: 'programs',
+    loadChildren: () => import('./program/program.module').then(module => module.ProgramModule)
+  },
+  {
+    path: 'classrooms',
+    loadChildren: () => import('./classroom/classroom.module').then(module => module.ClassroomModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
