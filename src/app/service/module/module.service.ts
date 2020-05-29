37dac6adcd0e5e1559d83afc9bb7461cec065926
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-29 12:30:25 +0700

    refactor

--- src/app/service/module/module.service.ts
+++ src/app/service/module/module.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {environment} from "../../environments/environment";
+import {environment} from "../../../environments/environment";
 import {Observable} from "rxjs";
-import {Module} from "../interface/module";
+import {Module} from "../../interface/module";
 
 const API_URL = `${environment.apiUrl}`
 
