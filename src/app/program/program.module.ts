e0ad38c2e65dea53489f50c11694247e702fc899
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-29 12:50:03 +0700

    add program module

---
+++ src/app/program/program.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ProgramRoutingModule} from "./program-routing.module";
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProgramRoutingModule
+  ]
+})
+export class ProgramModule { }
