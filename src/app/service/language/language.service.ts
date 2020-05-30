704eb841303d46e60f1fee5652c09ed622261740
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-05-30 10:02:12 +0700

    refactor

--- src/app/service/language/language.service.ts
+++ src/app/service/language/language.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import {environment} from "../../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
-import {Classroom} from "../../interface/classroom";
 import {Language} from "../../interface/language";
 
 const API_URL = `${environment.apiUrl}`
