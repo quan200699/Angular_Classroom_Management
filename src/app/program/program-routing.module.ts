1d57455df1794a08528e474f2f61f484fdf58813
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-06-03 10:04:51 +0700

    add student module

--- src/app/program/program-routing.module.ts
+++ src/app/program/program-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {ListProgramComponent} from "./list-program/list-program.component";
 import {CreateProgramComponent} from "./create-program/create-program.component";
