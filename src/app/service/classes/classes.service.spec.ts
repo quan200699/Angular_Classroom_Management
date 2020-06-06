98ba14fd368c4f6cae16b5329c7c4b00075ab056
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-06-06 22:20:57 +0700

    test getl all classes service has three item done

--- src/app/service/classes/classes.service.spec.ts
+++ src/app/service/classes/classes.service.spec.ts
@@ -1,12 +1,56 @@
-import { TestBed } from '@angular/core/testing';
+import {async, inject, TestBed} from '@angular/core/testing';
 
-import { ClassesService } from './classes.service';
+import {ClassesService} from './classes.service';
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {environment} from "../../../environments/environment";
+
+const API_URL = `${environment.apiUrl}`
 
 describe('ClassesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let classesService: ClassesService;
+  let httpMock: HttpTestingController;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ],
+      providers: [
+        ClassesService
+      ],
+    });
 
-  it('should be created', () => {
-    const service: ClassesService = TestBed.get(ClassesService);
-    expect(service).toBeTruthy();
+    classesService = TestBed.get(ClassesService);
+    httpMock = TestBed.get(HttpTestingController);
   });
+  it('should be created', () => {
+    expect(classesService).toBeTruthy();
+  });
+  it(`should fetch classes as an Observable`, async(inject([HttpTestingController, ClassesService],
+    (httpClient: HttpTestingController, classesService1: ClassesService) => {
+      const classes = [{
+        id: 1,
+        name: "C0320H1",
+        classTime: "H",
+        module: "Module 3"
+      }, {
+        id: 2,
+        name: "C0520H1",
+        classTime: "H",
+        module: "Module 2"
+      }, {
+        id: 3,
+        name: "C1219G1",
+        classTime: "G",
+        module: "Module 5"
+      },];
+      let req = httpMock.expectOne(API_URL + `/classes`);
+      expect(req.request.method).toBe("GET");
+      classesService1.getAllClasses()
+        .subscribe((listClass: any) => {
+          expect(listClass.length).toBe(3);
+        });
+      req.flush(classes);
+      httpMock.verify();
+    })));
 });
+
