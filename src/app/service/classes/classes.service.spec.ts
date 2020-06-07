07df1b38dd8b15d2de50719950a31fcb567c2ce4
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-06-07 08:25:41 +0700

    test get class by id done

--- src/app/service/classes/classes.service.spec.ts
+++ src/app/service/classes/classes.service.spec.ts
@@ -3,12 +3,14 @@ import {async, inject, TestBed} from '@angular/core/testing';
 import {ClassesService} from './classes.service';
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 import {environment} from "../../../environments/environment";
+import {Classes} from "../../interface/classes";
 
 const API_URL = `${environment.apiUrl}`
 
 describe('ClassesService', () => {
   let classesService: ClassesService;
   let httpMock: HttpTestingController;
+  let listClasses: Classes[] = [];
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -25,9 +27,9 @@ describe('ClassesService', () => {
   it('should be created', () => {
     expect(classesService).toBeTruthy();
   });
-  it(`should fetch classes as an Observable`, async(inject([HttpTestingController, ClassesService],
+  it(`should fetch classes as an Observable has length = 3`, async(inject([HttpTestingController, ClassesService],
     (httpClient: HttpTestingController, classesService1: ClassesService) => {
-      const classes = [{
+      listClasses = [{
         id: 1,
         name: "C0320H1",
         classTime: "H",
@@ -43,14 +45,31 @@ describe('ClassesService', () => {
         classTime: "G",
         module: "Module 5"
       },];
-      let req = httpMock.expectOne(API_URL + `/classes`);
-      expect(req.request.method).toBe("GET");
       classesService1.getAllClasses()
         .subscribe((listClass: any) => {
           expect(listClass.length).toBe(3);
+          expect(listClass).toEqual(listClasses)
         });
-      req.flush(classes);
-      httpMock.verify();
+      let req = httpMock.expectOne(API_URL + '/classes');
+      expect(req.request.method).toBe("GET");
+      req.flush(listClasses);
     })));
+  it('should return this object', async(inject([HttpTestingController, ClassesService],
+    (httpClient: HttpTestingController, classesService: ClassesService) => {
+    const classDetail = {
+      id: 3,
+      name: "C1219G1",
+      classTime: "G",
+      module: "Module 5"
+    };
+      classesService.getClasses(3).subscribe(classes => {
+        expect(classes).toEqual(classDetail)
+      })
+      let req = httpMock.expectOne(API_URL + `/classes/3`);
+      expect(req.request.method).toBe("GET");
+    })));
+  afterEach(() => {
+    httpMock.verify();
+  })
 });
 
