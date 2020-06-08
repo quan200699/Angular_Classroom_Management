4b5907847b4b8e75e59e74174015f1e6f9d55f80
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-06-08 16:38:22 +0700

    should be created passed!

--- src/app/service/job/job.service.spec.ts
+++ src/app/service/job/job.service.spec.ts
@@ -1,12 +1,31 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { JobService } from './job.service';
+import {JobService} from './job.service';
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 
 describe('JobService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let jobService: JobService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        JobService
+      ]
+    });
+    jobService = TestBed.get(JobService);
+    httpMock = TestBed.get(HttpTestingController);
+  });
 
   it('should be created', () => {
     const service: JobService = TestBed.get(JobService);
     expect(service).toBeTruthy();
   });
+
+  afterEach(() => {
+    httpMock.verify();
+  })
 });
